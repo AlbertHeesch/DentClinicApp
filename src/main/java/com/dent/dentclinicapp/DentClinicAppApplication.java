ca6faf92f675974e67e1dc13e99a35c5e3642ddb
aba22able <oddajgrzanki@gmail.com>
2022-09-18 21:29:21 +0200

    My First Commit

---
+++ src/main/java/com/dent/dentclinicapp/DentClinicAppApplication.java
@@ -0,0 +1,13 @@
+package com.dent.dentclinicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentClinicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentClinicAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dent/dentclinicapp/DentClinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dent.dentclinicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentClinicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
