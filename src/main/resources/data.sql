481fb326ba0247b78513b566b6c700549b1393d6
aheesch <a.heesch@kcmrm.pl>
2023-04-21 15:33:21 +0200

    data.sql refactor & hibernate data.sql configuration

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,10 @@
-insert into services (service_id, cost, service_description) values (1, 100, 'Follow up visit');
-insert into services (service_id, cost, service_description) values (2, 200, 'Tooth treatment per one');
-insert into services (service_id, cost, service_description) values (3, 400, 'Surgery tooth removal per one');
-insert into services (service_id, cost, service_description) values (4, 500, 'Teeth whitening');
+insert into services (service_id, cost, service_description) values
+                                                                 (1, 100, 'Follow up visit'),
+                                                                 (2, 200, 'Tooth treatment per one'),
+                                                                 (3, 400, 'Surgery tooth removal per one'),
+                                                                 (4, 500, 'Teeth whitening');
 
-insert into dentists (dentist_id, name, surname, EXPERIENCE) values (1, 'Andrzej', 'Bąk', '1999-09-21');
-insert into dentists (dentist_id, name, surname, EXPERIENCE) values (2, 'Izabela', 'Kloc', '1989-01-31');
-insert into dentists (dentist_id, name, surname, EXPERIENCE) values (3, 'Sasza', 'Krawczenko', '2022-03-04');
+insert into dentists (dentist_id, name, surname, EXPERIENCE) values
+                                                                 (1, 'Andrzej', 'Bąk', '1999-09-21'),
+                                                                 (2, 'Izabela', 'Kloc', '1989-01-31'),
+                                                                 (3, 'Sasza', 'Krawczenko', '2022-03-04');
